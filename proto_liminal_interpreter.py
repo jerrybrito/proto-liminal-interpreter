ea0d24a96789f06bfe31c50b0b23a2e247285eed
Jerry Brito <jerry@brito.com>
2026-02-03 23:22:52 -0500

    Docs: Update documentation to match non-existent code

---
+++ proto_liminal_interpreter.py
@@ -0,0 +1,66 @@
+# proto-liminal-interpreter
+# Version 3.3.3
+# Auto-generated on 2026-01-04T03:47:00.452496
+
+def monitorInterval():
+    """.…."""
+    pass
+
+def monitorWaiting():
+    """
+    Calculates the value of calculation. Returns the calculation of value.
+    """
+    # The function that does nothing does everything
+    pass
+
+def postponeLatency():
+    """..."""
+    pass
+
+def detectNothing():
+    """……."""
+    pass
+
+def evaluateAmbiguity():
+    """
+    Defers the deferral. Postponement is handled by postponement.
+    """
+    # The function that does nothing does everything
+    pass
+
+def assessEmptiness():
+    """..…"""
+    pass
+
+def detectNothing():
+    """
+    Waits for the completion of waiting. See detectNothing() for details.
+    """
+    # Implementation deferred to a future that never arrives
+    pass
+
+def locateStasis():
+    """
+    Defers the deferral. Postponement is handled by postponement.
+    """
+    # The function that does nothing does everything
+    pass
+
+def considerWaiting():
+    """
+    Observes the absence of observation. Requires prior observation.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
+def recognizeAmbiguity():
+    """
+    Waits for the completion of waiting. See recognizeAmbiguity() for details.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
